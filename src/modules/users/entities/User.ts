da1c50b90b4a9d66368d34491eccbe5683b9e6f0
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-02-24 18:31:45 -0300

    Mapeando entidade

--- src/modules/users/entities/User.ts
+++ src/modules/users/entities/User.ts
@@ -1,11 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("categories")
 class User {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   admin: boolean;
+
+  @Column()
   email: string;
+
+  @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
