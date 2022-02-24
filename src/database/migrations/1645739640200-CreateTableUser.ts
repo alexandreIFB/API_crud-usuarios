f4dba5e0958ba341053e0c8b0172aca19e5716c2
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-02-24 19:11:18 -0300

    Migration CreateTableUser

---
+++ src/database/migrations/1645739640200-CreateTableUser.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTableUser1645739640200 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "admin",
+            type: "boolean",
+            isNullable: false,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamptz",
+            isNullable: false,
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamptz",
+            isNullable: false,
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
